fcfa483214270cba5916c098a684ec9144805055
Andrew MacCuaig <st-andrew@users.noreply.github.com>
2014-10-21 14:57:02 -0400

    Modified README and added Controller files

---
+++ server/headers/Controller.h
@@ -0,0 +1,19 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <stddef.h>
+#include <string>
+#include <iostream>
+
+// #include "UI.h"
+// #include "ServerFacade.h"
+
+class Controller {
+    public:
+        Controller();
+        ~Controller();
+
+        void Run();
+    private:
+};
+#endif

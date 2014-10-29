29630a7b0ce81361680de8f82f264c77e98b4287
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2014-10-28 21:42:12 -0400

    Started cleaning up the serializer class. It will NOT compile yet!

--- cuTPSServer/Serializer.cc
+++ cuTPSServer/Serializer.cc
@@ -1,93 +1,89 @@
 #include "headers/Serializer.h"
 
-#include <stdexcept>
+using namespace std;
 
-// For this we will have to work with JSONs
-commands_t Serializer::deserialize(const QByteArray& inJson, void* retData) {
-        // for reference
-        // enum commands_t {ADD_CONTENT, ADD_INVOICE, GET_CONTENT};
+int Serializer::Deserialize(const QByteArray *in_json, commands_t *out_command, void *out_object) {
 
-        // Create a QJsonDocument from the QByteArray
-        QJsonDocument jdoc = QJsonDocument::fromJson(inJson);
-        QJsonObject json;
-        if (jdoc.isNull()) {
-                // TODO Add better logging
-                std::cout << "ERROR: deserialize()::Serializer.cc, parsed Json is invalid!!!" << std::endl;
-                return ERROR;
-        }
-        else
-                json = jdoc.object();
+  // Create a QJsonDocument from the QByteArray
+  QJsonDocument jdoc = QJsonDocument::fromJson(in_json);
+  QJsonObject json;
 
-        // What command is being asked of us?
-        // works json["command"].toString();
-        commands_t retCommand = static_cast<commands_t>( json["command"].toDouble() );
+  // Create a QJsonObject from the QJsonDocument
+  if (jdoc.isNull()) {
+    throw runtime_error("ERROR: Serializer::Deserialize(), Invalid JSON");
+    return ERROR;
+  }
+  else
+    json = jdoc.object();
 
-        // enum commands_t {ADD_CONTENT, ADD_INVOICE, GET_CONTENT};
-        switch (retCommand) {
-                case ADD_CONTENT:
-                        // if it's this one then we need construct a list of content
-                        this->createContent(json, retData);
-                        break;
-                case ADD_INVOICE:
-                        // if it's this one then we need to construct an invoice
-                        this->createInvoice(json, retData);
-                        break;
-                case GET_CONTENT:
-                        // if it's this one then we merely do nothing else and pass off the return value
-                        break;
-                default:
-                        // TODO Add proper logging
-                        std::cout << "ERROR: deserialize()::Serializer.cc, didn't get proper value for json[\"command\"]" << std::endl;
-                        return ERROR;
-        }
+  // What command is being asked of us?
+  *retCommand = static_cast<commands_t>( json["command"].toDouble() );
 
-        return retCommand;
+  switch (*retCommand) {
+    case ADD_CONTENT:
+      // If it's this one then we need construct a list of content
+      this->CreateContent(json, retData);
+      break;
+    case ADD_INVOICE:
+      // if it's this one then we need to construct an invoice
+      this->createInvoice(json, retData);
+      break;
+    case GET_CONTENT:
+      // if it's this one then we merely do nothing else and pass off the return value
+      break;
+    default:
+      // TODO Add proper logging
+      std::cout << "ERROR: deserialize()::Serializer.cc, didn't get proper value for json[\"command\"]" << std::endl;
+      return ERROR;
+  }
+
+  return retCommand;
 }
 
 // We will create a QJsonObject with given data
-const QByteArray* Serializer::serialize(const commands_t& command, int errorState, void* contentList) {
-        // we initialize an empty QJsonObject
-        // XXX NEW MEMORY HERE
-        QByteArray* retJson = new QByteArray();
-        QJsonObject json;
+const QByteArray *Serializer::serialize(const commands_t &command, int errorState, void *contentList) {
+  // we initialize an empty QJsonObject
+  // XXX NEW MEMORY HERE
+  QByteArray *retJson = new QByteArray();
+  QJsonObject json;
 
-        // TODO decide on the Json key names with the team
-        // For the first 2, we just return a Json to indicate a success or failure for the client
-        switch (command) {
-                case ADD_CONTENT:
-                        json["command"] = command;
-                        json["error"] = errorState;
-                        break;
-                case ADD_INVOICE:
-                        json["command"] = command;
-                        json["error"] = errorState;
-                        break;
-                case GET_CONTENT:
-                        json["command"] = command;
-                        json["error"] = errorState;
-                        // TODO Discuss how serializing a list of content should work along with what it actually contains
-                        json["contentList"] = this->serializeContent(contentList);
-                        break;
-                default:
-                        // TODO Add proper logging
-                        std::cout << "ERROR: serialize()::Serializer.cc, didn't get proper value for command argument" << std::endl;
-                        throw std::runtime_error("ERROR: serialize()::Serializer.cc, didn't get proper value for command argument");
-                        return retJson;
-        }
-        return retJson;
+  // TODO decide on the Json key names with the team
+  // For the first 2, we just return a Json to indicate a success or failure for the client
+  switch (command) {
+    case ADD_CONTENT:
+      json["command"] = command;
+      json["error"] = errorState;
+      break;
+    case ADD_INVOICE:
+      json["command"] = command;
+      json["error"] = errorState;
+      break;
+    case GET_CONTENT:
+      json["command"] = command;
+      json["error"] = errorState;
+      // TODO Discuss how serializing a list of content should work along with what it actually contains
+      json["contentList"] = this->serializeContent(contentList);
+      break;
+    default:
+      // TODO Add proper logging
+      std::cout << "ERROR: serialize()::Serializer.cc, didn't get proper value for command argument" << std::endl;
+      throw std::runtime_error("ERROR: serialize()::Serializer.cc, didn't get proper value for command argument");
+      return retJson;
+  }
+  return retJson;
 }
 
 // Construct content
-void Serializer::createContent(const QJsonObject&, void*) {
+void Serializer::createContent(const QJsonObject &, void *) {
 }
 
 // Construct Invoice
-void Serializer::createInvoice(const QJsonObject&, void*) {
+void Serializer::createInvoice(const QJsonObject &, void *) {
 }
 
 // Create a Json array or object for all the content
-int Serializer::serializeContent(void*) {
-        return 0;
+int Serializer::serializeContent(void *) {
+  return 0;
 }
 
 
--- cuTPSServer/headers/Serializer.h
+++ cuTPSServer/headers/Serializer.h
@@ -1,62 +1,50 @@
 #ifndef SERIALIZER_H
 #define SERIALIZER_H
 
-<<<<<<< HEAD:server/headers/Serializer.h
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <QByteArray>
 
-#include "headers/Definitions.h"
-
-// #include "UI.h"
-// #include "ServerFacade.h"
-
-class Serializer {
-        public:
-                Serializer();
-                ~Serializer();
-
-                // TODO debate if deserialize or serialize should be const?
-
-                // public functions
-                
-                // enum commands_t {ADD_CONTENT, ADD_INVOICE, GET_CONTENT};
-
-                // TODO See if the input type should be a string or just a QJsonObject same for serialize
-                // Perhaps should be given QByteArray as we are working with Qt and we are probably going to send and receive with that datatype
-                
-                // 1st argument: give the Json object to be deserialized
-                // 2nd argument: being possible data created
-                // The return type being the commands_t enum as defined in headers/Definitions.h
-                commands_t deserialize(const QByteArray&, void*);
-
-                // 1st argument: give the command on what to ask of serializer
-                // 2nd argument: give successful or failure from the requested operation from deserialize
-                // 3rd argument: give the content list if was requested
-                // return: returned Json object to send back to client
-                const QByteArray* serialize(const commands_t&, int, void* = 0);
-
-        private:
-                void createContent(const QJsonObject&, void*);
-                void createInvoice(const QJsonObject&, void*);
-                // FIXME Change the return type to a content list
-                int serializeContent(void*);
-=======
-#include <string>
+#include "../../common/headers/Definitions.h"
 
 using namespace std;
 
 class Serializer {
-    public:
-        Serializer();
-        ~Serializer();
+  public:
+    Serializer();
+    ~Serializer();
 
-	// public functions
-    int deserialize(string*, string*, void*);
-    int serialize(int, void*, string*);
-    private:
->>>>>>> f8c780e78b034f08413f6951af755180882b7a64:cuTPSServer/headers/Serializer.h
+    /* =====================================================================
+    Function  : Deserialize
+    Purpose   : Deserializes a request
+    Variables : In  - The raw data from the Connection class
+                Out - The command to be performed
+                Out - The deserialized object
+    Returns   : 1 - Success
+                0 - Fail
+    ===================================================================== */
+    int Deserialize(const QByteArray *, commands_t *, void *) const;
+
+    /* =====================================================================
+    Function  : Serialize
+    Purpose   : Deserializes a request
+    Variables : In  - The command that was performed
+                In  - The object to serialize
+                In  - The outcome of the operation (1 = Success, 0 = Fail)
+                Out - The serialized object to send to the connection class
+    Returns   : 1 - Success
+                0 - Fail
+    ===================================================================== */
+    int Serialize(const commands_t &, void, int, QJsonObject *) const;
+
+  private:
+    void CreateContent(const QJsonObject &, void *);
+    void createInvoice(const QJsonObject &, void *);
+    // FIXME Change the return type to a content list
+    int serializeContent(void *);
 };
 #endif

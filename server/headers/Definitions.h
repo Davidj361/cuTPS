46beef0c46e4996dfb8a5f82195e8d679339d1d0
David Jatczak <david.j.361@gmail.com>
2014-10-28 21:48:06 -0400

    Got rid of old server/ folder since it was a mistake

--- server/headers/Definitions.h
+++
@@ -1,7 +0,0 @@
-#ifndef DEFINITIONS_H
-#define DEFINITIONS_H
-
-// These are the enumerations we are using for communication between Serializer and Controller
-enum commands_t {ADD_CONTENT, ADD_INVOICE, GET_CONTENT, ERROR};
-
-#endif

a81ddd672ad38feff99f829681b81f4139e30de8
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2014-11-01 13:54:59 -0400

    fixed db errors

--- resources/cuTPS.sql
+++ resources/cuTPS.sql
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `Chapters` (
 );
 
 CREATE TABLE IF NOT EXISTS `Sections` (
-    `id` INTEGER UNIQUE,
+-- exi    `id` INTEGER UNIQUE,
     `name` TEXT NOT NULL,
     `number` INTEGER NOT NULL,
     `chapter` INTEGER NOT NULL,

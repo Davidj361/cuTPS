750591dc4d16a9036205c43261e3010ecdd7da05
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2014-11-02 16:45:30 -0500

    Working on test cases

--- cuTPSClient/MainWindow.cpp
+++ cuTPSClient/MainWindow.cpp
@@ -17,7 +17,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
   connect(ui->btnRunTest2,    SIGNAL(clicked()),   this, SLOT(runTest2()));
   connect(ui->btnRunTest3,    SIGNAL(clicked()),   this, SLOT(runTest3()));
 
-  connect(connection, SIGNAL(ConnectionError(QString)), this, SLOT(displayError()));
+  connect(connection, SIGNAL(ConnectionError(QString)), this, SLOT(displayError(QString)));
 }
 
 MainWindow::~MainWindow() {
@@ -57,6 +57,8 @@ void MainWindow::runTest1() {
   QListWidgetItem *test3 = new QListWidgetItem; // Add section test
 
   ui->resultsListWidget->addItem(test1);
+  ui->resultsListWidget->addItem(test2);
+  ui->resultsListWidget->addItem(test3);
 
   QString result;
   QString errorMsg;
@@ -85,18 +87,54 @@ void MainWindow::runTest1() {
           test1->setText( test1->text() + "FAILED" );
           test1->setForeground(QBrush(Qt::red));
       }
+  }
+  catch (exception &e) {
+      qDebug() << e.what();
+  }
+
+  try {
+      test2->setText("Performing add chapter test...");
 
       qDebug() << "=========================";
       serializer->Serialize(ADD_CHAPTER, c, REQUEST, req);
       qDebug() << req;
       connection->request(req, res);
       qDebug() << res;
+      serializer->Deserialize(res, object, result, errorMsg);
+
+      qDebug() << "Result: " << result;
+      if (result == "success") {
+          test2->setText( test2->text() + "PASSED" );
+          test2->setForeground(QBrush(Qt::green));
+      }
+      else {
+          test2->setText( test2->text() + "FAILED" );
+          test2->setForeground(QBrush(Qt::red));
+      }
+  }
+  catch (exception &e) {
+      qDebug() << e.what();
+  }
+
+  try {
+      test3->setText("Performing add section test...");
 
       qDebug() << "=========================";
       serializer->Serialize(ADD_SECTION, s, REQUEST, req);
       qDebug() << req;
       connection->request(req, res);
       qDebug() << res;
+      serializer->Deserialize(res, object, result, errorMsg);
+
+      qDebug() << "Result: " << result;
+      if (result == "success") {
+          test3->setText( test3->text() + "PASSED" );
+          test3->setForeground(QBrush(Qt::green));
+      }
+      else {
+          test3->setText( test3->text() + "FAILED" );
+          test3->setForeground(QBrush(Qt::red));
+      }
   }
   catch (exception &e) {
       qDebug() << e.what();
@@ -106,15 +144,48 @@ void MainWindow::runTest1() {
 }
 
 void MainWindow::runTest2() {
-  qDebug() << "Running test 2";
-  User user("peter","", "", "");
-  QByteArray res;
-  QByteArray req;
+  qDebug() << "Running test 2 - Retrieve Content";
 
-  serializer->Serialize(GET_CONTENT, &user, REQUEST, req);
-  qDebug() << req;
-  connection->request(req, res);
-  qDebug() << res;
+  ui->btnRunTest2->setEnabled(false);
+
+  QListWidgetItem *test1 = new QListWidgetItem; // Retrieve content
+
+  ui->resultsListWidget->addItem(test1);
+
+  QString result;
+  QString errorMsg;
+  void *object = 0;
+
+  User user("peter","", "", "");
+  vector<Textbook*> book_list;
+
+  try {
+      test1->setText ("Performing Retrieve Content test...");
+      serializer->Serialize(GET_CONTENT, &user, REQUEST, req);
+      qDebug() << req;
+      connection->request(req, res);
+      qDebug() << res;
+      serializer->Deserialize(res, object, result, errorMsg);
+      book_list = *(static_cast<vector<Textbook*>*>(object));
+
+      qDebug() << "Result: " << result;
+      if (result == "success") {
+          test1->setText( test1->text() + "PASSED" );
+          test1->setForeground(QBrush(Qt::green));
+      }
+      else {
+          test1->setText( test1->text() + "FAILED" );
+          test1->setForeground(QBrush(Qt::red));
+      }
+
+  }
+  catch (exception &e) {
+      test1->setText( test1->text() + "FAILED - " + e.what() );
+      test1->setForeground(QBrush(Qt::red));
+      qDebug() << e.what();
+  }
+
+  ui->btnRunTest2->setEnabled(true);
 }
 
 void MainWindow::runTest3() {

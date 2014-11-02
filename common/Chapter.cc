5cbd5a98e351ec8d42a67c28ae352af8ddd5d8b4
David Jatczak <david.j.361@gmail.com>
2014-11-02 17:25:26 -0500

    Made Serializer functions in entity classes have proper JSon. Added DEBUG preprocessor stuff.

--- common/Chapter.cc
+++ common/Chapter.cc
@@ -1,38 +1,40 @@
 #include "headers/Chapter.h"
 
 Chapter::Chapter(QString cTitle, int cChapterno, Textbook *cParent,
-                 QString cDescription, bool cAvailable, float cPrice, int cC_id) {
-  title = cTitle;
-  available = cAvailable;
-  price = cPrice;
-  description = cDescription;
-  chapterNo = cChapterno;
-  textbook = cParent;
-  c_id = cC_id;
+                QString cDescription, bool cAvailable, float cPrice, int cC_id) {
+        title = cTitle;
+        available = cAvailable;
+        price = cPrice;
+        description = cDescription;
+        chapterNo = cChapterno;
+        textbook = cParent;
+        c_id = cC_id;
 }
 
 Textbook* Chapter::getTextbook() {
-  return textbook;
+        return textbook;
 }
 
 void Chapter::serialize(QJsonObject & chapterJson){
+        QJsonObject content;
 
-    chapterJson["title"] = title;
-    chapterJson["available"] = available;
-    chapterJson["price"] = price;
-    chapterJson["chapterNo"] = chapterNo;
-    chapterJson["description"] = description;
-    chapterJson["c_id"] = c_id;
-    chapterJson["ISBN"] = textbook->getISBN();
+        content["title"] = title;
+        content["available"] = available;
+        content["price"] = price;
+        content["chapterNo"] = chapterNo;
+        content["description"] = description;
+        content["c_id"] = c_id;
+        content["ISBN"] = textbook->getISBN();
 
+        chapterJson["content"] = content;
 }
 
 void Chapter::addSection(Section* sec){
-  sections.push_back(sec);
+        sections.push_back(sec);
 }
 
 vector<Section*>& Chapter::getSections(){return this->sections;}
 
 int Chapter::getChapterNo(){
-    return chapterNo;
+        return chapterNo;
 }

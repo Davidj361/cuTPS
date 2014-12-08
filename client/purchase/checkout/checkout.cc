5a5b3fd37c3f6dde40c7f822360a9c73da80a89e
David Jatczak <david.j.361@gmail.com>
2014-12-08 14:38:35 -0500

    Made pointers properly const in Checkout.

--- client/purchase/checkout/checkout.cc
+++ client/purchase/checkout/checkout.cc
@@ -1,6 +1,6 @@
 #include "checkout.h"
 
-Checkout::Checkout(const StorageControl& storage, const ShoppingCart& cart, const LocalStorage& lStorage) : storage(&storage), cart(&cart), lStorage(&lStorage) {}
+Checkout::Checkout(const StorageControl& storage, ShoppingCart& cart, const LocalStorage& lStorage) : storage(&storage), cart(&cart), lStorage(&lStorage) {}
 
 // This will send off the invoice object to StorageControl
 void Checkout::checkout() {
@@ -10,6 +10,7 @@ void Checkout::checkout() {
         Invoice invoice(lStorage->getUser().getUsername(), cart->getCartCids());
         try{
             storage->checkout(invoice);
+            cart->clearCart();
         }
         catch(std::runtime_error e){
             throw e;
